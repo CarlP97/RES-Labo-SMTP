a830b90547e2d9fcd631d3fc3a4e0b2706609175
Carl <carl.penalva@heig-vd.ch>
2021-04-30 22:42:06 +0200

    add files for model mail and config

--- PrankMailSender/src/main/java/ch/heigvd/res/pranksender/config/IConfigurationManager.java
+++ PrankMailSender/src/main/java/ch/heigvd/res/pranksender/config/IConfigurationManager.java
@@ -1,4 +1,14 @@
 package ch.heigvd.res.pranksender.config;
 
+import ch.heigvd.res.pranksender.model.mail.Person;
+
+import java.util.List;
+
 public interface IConfigurationManager {
+    List<Person> getVictims();
+    List<String> getMessages();
+    int getNumberOfGroups();
+    String getSmtpServerAddress();
+    int getSmtpServerPort();
+    List<Person> getCcs();
 }
--- PrankMailSender/src/main/java/ch/heigvd/res/pranksender/model/mail/Message.java
+++ PrankMailSender/src/main/java/ch/heigvd/res/pranksender/model/mail/Message.java
@@ -1,4 +1,19 @@
 package ch.heigvd.res.pranksender.model.mail;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Message {
+    private String from;
+    private String[] to;
+    private String[] cc;
+    private String[] bcc;
+    private String subject;
+    private String body;
 }
