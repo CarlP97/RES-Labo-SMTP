29a5779c61481430c1ff7e93490c3d2144bfde4d
Carl <carl.penalva@heig-vd.ch>
2021-04-30 15:48:04 +0200

    enfin une reussite

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Ask maven to build the executable jar file from the source files
-mvn clean install --file ../mockmockserver/MockMock/pom.xml
-
-# Copy the executable jar file in the current directory
-cp ../mockmockserver/MockMock/target/MockMock-1.4.0.one-jar.jar .
-
 # Build the Docker image locally
 docker build --tag mockmock-server .
